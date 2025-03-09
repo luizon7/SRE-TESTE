b3494f491a9fca5f895446884f58a6cd738b65d0
luizon07 <luiz.silveira29@outlook.com>
2025-03-09 14:57:23 -0300

    ajuste dos descriptions

--- GKE-HW/variables.tf
+++ GKE-HW/variables.tf
@@ -1,28 +1,28 @@
 variable "mb_hello_project_id" {
-  description = "ID do projeto GCP para o hello-world da MercadoBitcoin"
+  description = "ID do projeto GCP MercadoBitcoin"
   type        = string
 }
 
 variable "mb_hello_region" {
-  description = "Região onde o cluster GKE será criado"
+  description = "Região do cluster GKE"
   type        = string
   default     = "us-central1"
 }
 
 variable "mb_hello_cluster_name" {
-  description = "Nome do cluster GKE para o projeto hello-world"
+  description = "Nome do cluster GKE"
   type        = string
   default     = "mb-hello-cluster"
 }
 
 variable "mb_hello_node_count" {
-  description = "Quantidade de nós no node pool"
+  description = "Quantidade de nós"
   type        = number
   default     = 3
 }
 
 variable "mb_hello_machine_type" {
-  description = "Tipo de máquina dos nós do cluster"
+  description = "Tipo de máquina do cluster"
   type        = string
   default     = "e2-medium"
 }
