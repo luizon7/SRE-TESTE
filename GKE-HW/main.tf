e3c0c473eb127a60e295eaf2de47383917b8234e
luizon07 <luiz.silveira29@outlook.com>
2025-03-09 16:02:26 -0300

    ajustando recurso

--- GKE-HW/main.tf
+++ GKE-HW/main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project = var.mb_hello_project_id
+  region  = var.mb_hello_region
 }
 
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
+resource "google_container_cluster" "mb_hello" {
+  name     = var.mb_hello_cluster_name
+  location = var.mb_hello_region
 
   deletion_protection = false
 
@@ -16,14 +16,14 @@ resource "google_container_cluster" "primary" {
   ip_allocation_policy {}
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.node_count
+resource "google_container_node_pool" "mb_hello_nodes" {
+  name       = "${var.mb_hello_cluster_name}-node-pool"
+  location   = var.mb_hello_region
+  cluster    = google_container_cluster.mb_hello.name
+  node_count = var.mb_hello_node_count
 
   node_config {
-    machine_type = var.machine_type
+    machine_type = var.mb_hello_machine_type
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
